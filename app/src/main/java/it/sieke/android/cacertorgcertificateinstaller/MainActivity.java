13e64afb97a47b62ff0aa6fefb1f211fb53e9459
Matthias Sieke <matthias@sieke.it>
2014-07-09 13:52:47 +0200

    AsyncTask

--- app/src/main/java/it/sieke/android/cacertorgcertificateinstaller/MainActivity.java
+++ app/src/main/java/it/sieke/android/cacertorgcertificateinstaller/MainActivity.java
@@ -3,12 +3,13 @@ package it.sieke.android.cacertorgcertificateinstaller;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
 import android.view.*;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 import java.io.FileInputStream;
@@ -40,41 +41,33 @@ public class MainActivity extends Activity {
 
     Button installButton;
     TextView textViewLog;
+    boolean operationInProgress = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         installButton = (Button) findViewById(R.id.addCertButton);
         textViewLog = (TextView) findViewById(R.id.textViewLog);
+
 
         packageName = this.getPackageName();
         certsInstalled = checkCertInstalled();
         refreshInstallMode();
 
-        AlertDialog ad = new AlertDialog.Builder(this).create();
-        ad.setCancelable(false); // This blocks the 'BACK' button
-        ad.setMessage("This app is licensed under GPL. However you should not trust it blindly and check if the correct certificates were added.");
-        ad.setButton("OK", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.dismiss();
-            }
-        });
-        ad.show();
 
-        if (!RootTools.isRootAvailable()) {
-            //TODO root not available!
-        } else {
-            writeLog("found su");
-        }
-
-        if (!RootTools.isAccessGiven()) {
-            //TODO no access
-            Log.w(packageName, Environment.getRootDirectory().toString() + "NOOOOOO ACCESS!!!! :-(");
-        } else {
-            writeLog("root access was granted");
+        if (dieIfRootUnavailable()) {
+            AlertDialog ad = new AlertDialog.Builder(this).create();
+            ad.setMessage("This app is licensed under GPL. However, you should not trust it blindly and check if the correct certificates were added.");
+            ad.setButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                }
+            });
+            ad.show();
         }
 
 
@@ -85,49 +78,34 @@ public class MainActivity extends Activity {
         });
     }
 
-    private void installButtonClicked() {
-        boolean certsInstalledOld = certsInstalled;
-        if (!certsInstalled) {
-            try {
-                writeLog("starting to install certificates");
-                installCerts();
-            } catch (IOException e) {
-                Log.w(packageName, "error: " + e.getMessage());
-            } catch (RootDeniedException e) {
-                Log.w(packageName, "error: " + e.getMessage());
-            } catch (TimeoutException e) {
-                Log.w(packageName, "error: " + e.getMessage());
-            }
+    private boolean dieIfRootUnavailable() {
+        if (!RootTools.isRootAvailable() || !RootTools.isAccessGiven()) {
+            AlertDialog ad = new AlertDialog.Builder(this).create();
+            ad.setCancelable(false); // This blocks the 'BACK' button
+            ad.setMessage("This app requires root access.\n\nBecause root seems not to be available, or root access was not granted it will now quit.");
+            ad.setButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                    System.exit(0);
+                }
+            });
+            ad.show();
         } else {
-            try {
-                writeLog("starting to uninstall certificates");
-                uninstallCerts();
-            } catch (IOException e) {
-                Log.w(packageName, "error: " + e.getMessage());
-            } catch (RootDeniedException e) {
-                Log.w(packageName, "error: " + e.getMessage());
-            } catch (TimeoutException e) {
-                Log.w(packageName, "error: " + e.getMessage());
-            }
+            writeLog("root access was granted");
+            return true;
         }
+        return false;
+    }
 
-        try {
-            Thread.sleep(300);
-        } catch (InterruptedException e) {
+
+    private void installButtonClicked() {
+        if (operationInProgress) {
+            writeLog("Please wait. Operation in progress!");
+            return;
         }
-
-        certsInstalled = checkCertInstalled();
-
-        if (!certsInstalledOld && certsInstalled) {
-            writeLog("=== FINISH === " + System.getProperty("line.separator") + "everything went fine, certificates are now installed!");
-        }
-
-        if (certsInstalledOld && !certsInstalled) {
-            writeLog("=== FINISH === " + System.getProperty("line.separator") + "everything went fine, certificates are now removed!");
-        }
-
-        refreshInstallMode();
-
+        operationInProgress = true;
+        new InstallerTask().execute(!certsInstalled);
     }
 
     private void refreshInstallMode() {
@@ -138,26 +116,17 @@ public class MainActivity extends Activity {
         }
     }
 
+    private void checkInstallSuccess() {
+        boolean certsInstalledOld = certsInstalled;
+        certsInstalled = checkCertInstalled();
 
-    private void installCerts() throws IOException, TimeoutException, RootDeniedException {
-        writeLog("writing temporary files to sdcard");
+        if (certsInstalledOld != certsInstalled) {
+            writeLog("=== FINISH === " + System.getProperty("line.separator") + "everything went fine");
+        } else {
+            writeLog("=== ERROR === " + System.getProperty("line.separator") + "the operation was not successful");
+        }
 
-        moveRawFileToSdCard(R.raw.cert_5ed36f99_0, "5ed36f99.0");
-        moveRawFileToSdCard(R.raw.cert_e5662767_0, "e5662767.0");
-
-        writeLog("remounting system partition for writing");
-        executeCommand("su -c 'mount -o remount,rw /system'");
-
-        writeLog("copying certificates to system partition");
-        executeCommand("su -c 'cat " + Environment.getExternalStorageDirectory() + "/5ed36f99.0 > /system/etc/security/cacerts/5ed36f99.0'");
-        executeCommand("su -c 'cat " + Environment.getExternalStorageDirectory() + "/e5662767.0 > /system/etc/security/cacerts/e5662767.0'");
-
-        writeLog("writing file permissions");
-        executeCommand("su -c 'chmod 644 /system/etc/security/cacerts/5ed36f99.0'");
-        executeCommand("su -c 'chmod 644 /system/etc/security/cacerts/e5662767.0'");
-
-        writeLog("remounting system partition to read only");
-        executeCommand("su -c 'mount -o remount,ro /system'");
+        refreshInstallMode();
     }
 
     private void moveRawFileToSdCard(int ressource, String targetFilename) throws IOException {
@@ -196,19 +165,6 @@ public class MainActivity extends Activity {
         };
 
         RootTools.getShell(true).add(cmd);
-    }
-
-
-    private void uninstallCerts() throws IOException, RootDeniedException, TimeoutException {
-        writeLog("remounting system partition for writing");
-        executeCommand("su -c 'mount -o remount,rw /system'");
-
-        writeLog("removing certificate files");
-        executeCommand("su -c 'rm /system/etc/security/cacerts/5ed36f99.0'");
-        executeCommand("su -c 'rm /system/etc/security/cacerts/e5662767.0'");
-
-        writeLog("remounting system partition to read only");
-        executeCommand("su -c 'mount -o remount,ro /system'");
     }
 
     private boolean checkCertInstalled() {
@@ -275,6 +231,96 @@ public class MainActivity extends Activity {
     }
 
     private void writeLog(String text) {
-        textViewLog.setText(text + System.getProperty("line.separator") + System.getProperty("line.separator") + textViewLog.getText());
+        textViewLog.setText(textViewLog.getText() + System.getProperty("line.separator") + System.getProperty("line.separator") + text);
+        final ScrollView sc = (ScrollView) findViewById(R.id.scrollView);
+        sc.post(new Runnable() {
+            @Override
+            public void run() {
+                sc.fullScroll(ScrollView.FOCUS_DOWN);
+            }
+        });
+    }
+
+
+    public class InstallerTask extends AsyncTask<Boolean, String, Void> {
+        TextView textViewLog;
+
+        @Override
+        protected void onPreExecute() {
+            textViewLog = (TextView) findViewById(R.id.textViewLog);
+        }
+
+        @Override
+        protected Void doInBackground(Boolean... params) {
+            boolean install = params[0];
+
+            try {
+                if (install) {
+                    installCerts();
+                } else {
+                    uninstallCerts();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (TimeoutException e) {
+                e.printStackTrace();
+            } catch (RootDeniedException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(String... values) {
+            MainActivity.this.writeLog(values[0]);
+        }
+
+        private void installCerts() throws IOException, TimeoutException, RootDeniedException {
+            publishProgress("writing temporary files to sdcard");
+
+            moveRawFileToSdCard(R.raw.cert_5ed36f99_0, "5ed36f99.0");
+            moveRawFileToSdCard(R.raw.cert_e5662767_0, "e5662767.0");
+
+            publishProgress("remounting system partition for writing");
+            executeCommand("su -c 'mount -o remount,rw /system'");
+
+            publishProgress("copying certificates to system partition");
+            executeCommand("su -c 'cat " + Environment.getExternalStorageDirectory() + "/5ed36f99.0 > /system/etc/security/cacerts/5ed36f99.0'");
+            executeCommand("su -c 'cat " + Environment.getExternalStorageDirectory() + "/e5662767.0 > /system/etc/security/cacerts/e5662767.0'");
+
+            publishProgress("writing file permissions");
+            executeCommand("su -c 'chmod 644 /system/etc/security/cacerts/5ed36f99.0'");
+            executeCommand("su -c 'chmod 644 /system/etc/security/cacerts/e5662767.0'");
+
+            publishProgress("remounting system partition to read only");
+            executeCommand("su -c 'mount -o remount,ro /system'");
+        }
+
+
+        private void uninstallCerts() throws IOException, RootDeniedException, TimeoutException {
+            publishProgress("remounting system partition for writing");
+            executeCommand("su -c 'mount -o remount,rw /system'");
+
+            publishProgress("removing certificate files");
+            executeCommand("su -c 'rm /system/etc/security/cacerts/5ed36f99.0'");
+            executeCommand("su -c 'rm /system/etc/security/cacerts/e5662767.0'");
+
+            publishProgress("remounting system partition to read only");
+            executeCommand("su -c 'mount -o remount,ro /system'");
+        }
+
+        protected void onPostExecute(Void result) {
+            try {
+                Thread.sleep(300);
+            } catch (InterruptedException e) {
+            }
+
+            operationInProgress = false;
+
+            checkInstallSuccess();
+        }
+
+
     }
 }
