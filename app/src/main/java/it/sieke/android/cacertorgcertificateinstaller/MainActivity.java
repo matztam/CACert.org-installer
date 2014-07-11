2d045dafeec37139cfb1e442f62927d3f11b0e72
Matthias Sieke <matthias@sieke.it>
2014-07-11 20:01:08 +0200

    test

--- app/src/main/java/it/sieke/android/cacertorgcertificateinstaller/MainActivity.java
+++ app/src/main/java/it/sieke/android/cacertorgcertificateinstaller/MainActivity.java
@@ -263,6 +263,8 @@ public class MainActivity extends Activity {
         return sb.toString();
     }*/
 
+    /*test*/
+
     private void writeLog(String text) {
         textViewLog.setText(textViewLog.getText() + "\n\n" + text);
         final ScrollView sc = (ScrollView) findViewById(R.id.scrollView);
