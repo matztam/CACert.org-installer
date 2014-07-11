71b7bf6a64222c896b3a9fc1da39c9aa015f0ee4
Matthias Sieke <matthias@sieke.it>
2014-07-11 18:05:38 +0200

    some new graphics X509 fingerprint checking strings moved to strings.xml

--- app/src/main/java/it/sieke/android/cacertorgcertificateinstaller/MainActivity.java
+++ app/src/main/java/it/sieke/android/cacertorgcertificateinstaller/MainActivity.java
@@ -25,41 +25,43 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
-import android.view.*;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
+
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.security.MessageDigest;
-import java.io.File;
-import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateEncodingException;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
 import java.util.concurrent.TimeoutException;
 
-
-import com.stericson.RootTools.*;
+import com.stericson.RootTools.RootTools;
 import com.stericson.RootTools.exceptions.RootDeniedException;
 import com.stericson.RootTools.execution.Command;
 
-
 public class MainActivity extends Activity {
 
+    private static final String CERTFILE1_PATH = "/system/etc/security/cacerts/5ed36f99.0";
+    private static final String CERTFILE1_FINGERPRINT = "a6:1b:37:5e:39:0d:9c:36:54:ee:bd:20:31:46:1f:6b";
+    private static final String CERTFILE2_PATH = "/system/etc/security/cacerts/e5662767.0";
+    private static final String CERTFILE2_FINGERPRINT = "f7:25:12:82:4e:67:b5:d0:8d:92:b7:7c:0b:86:7a:42";
     public static String packageName;
-
-    private static String certFile1Path = "/system/etc/security/cacerts/5ed36f99.0";
-    private static String certFile1Md5 = "fb262d55709427e2e9acadf2c1298c99";
-
-    private static String certFile2Path = "/system/etc/security/cacerts/e5662767.0";
-    private static String certFile2Md5 = "95c1c1820c0ed1de88d512cb10e25182";
-
-    private boolean certsInstalled = false;
-
     Button installButton;
     TextView textViewLog;
     boolean operationInProgress = false;
+    private boolean certsInstalled = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -73,21 +75,21 @@ public class MainActivity extends Activity {
 
         packageName = this.getPackageName();
         certsInstalled = checkCertInstalled();
-        refreshInstallMode();
 
 
         if (dieIfRootUnavailable()) {
-            AlertDialog ad = new AlertDialog.Builder(this).create();
-            ad.setMessage("This app is licensed under GPL. However, you should not trust it blindly and check if the correct certificates were added.");
-            ad.setButton("OK", new DialogInterface.OnClickListener() {
+            AlertDialog adWelcome = new AlertDialog.Builder(this).create();
+            adWelcome.setMessage(this.getString(R.string.welcome_message));
+            adWelcome.setButton(this.getString(R.string.btOk), new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialog, int which) {
                     dialog.dismiss();
                 }
             });
-            ad.show();
+            adWelcome.show();
         }
 
+        refreshInstallMode();
 
         installButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -97,143 +99,6 @@ public class MainActivity extends Activity {
 
 
     }
-
-    private void showAboutDialog() {
-
-    }
-
-    private boolean dieIfRootUnavailable() {
-        if (!RootTools.isRootAvailable() || !RootTools.isAccessGiven()) {
-            AlertDialog ad = new AlertDialog.Builder(this).create();
-            ad.setCancelable(false); // This blocks the 'BACK' button
-            ad.setMessage("This app requires root access.\n\nBecause root seems not to be available, or root access was not granted it will now quit.");
-            ad.setButton("OK", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    dialog.dismiss();
-                    System.exit(0);
-                }
-            });
-            ad.show();
-        } else {
-            writeLog("root access was granted");
-            return true;
-        }
-        return false;
-    }
-
-
-    private void installButtonClicked() {
-        if (operationInProgress) {
-            writeLog("Please wait. Operation in progress!");
-            return;
-        }
-        operationInProgress = true;
-        new InstallerTask().execute(!certsInstalled);
-    }
-
-    private void refreshInstallMode() {
-        if (certsInstalled) {
-            installButton.setText(R.string.delCertButtonText);
-        } else {
-            installButton.setText(R.string.addCertButtonText);
-        }
-    }
-
-    private void checkInstallSuccess() {
-        boolean certsInstalledOld = certsInstalled;
-        certsInstalled = checkCertInstalled();
-
-        if (certsInstalledOld != certsInstalled) {
-            writeLog("=== FINISH === " + System.getProperty("line.separator") + "everything went fine");
-        } else {
-            writeLog("=== ERROR === " + System.getProperty("line.separator") + "the operation was not successful");
-        }
-
-        refreshInstallMode();
-    }
-
-    private void moveRawFileToSdCard(int ressource, String targetFilename) throws IOException {
-        InputStream in = getResources().openRawResource(ressource);
-        FileOutputStream out = new FileOutputStream(Environment.getExternalStorageDirectory() + "/" + targetFilename);
-
-        byte[] buff = new byte[1024];
-        int read = 0;
-        while ((read = in.read(buff)) > 0) {
-            out.write(buff, 0, read);
-        }
-        in.close();
-        out.close();
-    }
-
-    private void executeCommand(String command) throws TimeoutException, RootDeniedException, IOException {
-        Log.w(packageName, "executing \"" + command + "\"");
-
-        Command cmd = new Command(0, command) {
-            @Override
-            public void commandOutput(int i, String s) {
-                if (!s.isEmpty())
-                    Log.w(packageName, "command output: \"" + s + "\"");
-            }
-
-            @Override
-            public void commandTerminated(int i, String s) {
-                if (!s.isEmpty())
-                    Log.w(packageName, "command terminated: \"" + s + "\"");
-            }
-
-            @Override
-            public void commandCompleted(int i, int i2) {
-            }
-
-        };
-
-        RootTools.getShell(true).add(cmd);
-    }
-
-    private boolean checkCertInstalled() {
-        File certFile1 = new File(certFile1Path);
-        File certFile2 = new File(certFile2Path);
-
-        if (!certFile1.exists() || !certFile2.exists()) {
-            return false;
-        }
-
-        try {
-            if (!getFileMd5(certFile1).equals(certFile1Md5) || !getFileMd5(certFile2).equals(certFile2Md5)) {
-                return false;
-            }
-        } catch (IOException e) {
-            Log.w(packageName, "error: " + e.getMessage());
-        } catch (NoSuchAlgorithmException e) {
-            Log.w(packageName, "error: " + e.getMessage());
-        }
-
-        return true;
-    }
-
-    private String getFileMd5(File file) throws IOException, NoSuchAlgorithmException {
-        FileInputStream fileInputStream = new FileInputStream(file.getPath());
-        MessageDigest messageDigest = MessageDigest.getInstance("MD5");
-
-        //Using MessageDigest update() method to provide input
-        byte[] buffer = new byte[8192];
-        int numOfBytesRead;
-        while ((numOfBytesRead = fileInputStream.read(buffer)) > 0) {
-            messageDigest.update(buffer, 0, numOfBytesRead);
-        }
-
-        byte[] digestResult = messageDigest.digest();
-
-
-        StringBuffer sb = new StringBuffer();
-        for (int i = 0; i < digestResult.length; i++)
-            sb.append(Integer.toString((digestResult[i] & 0xff) + 0x100, 16).substring(1));
-
-
-        return sb.toString();
-    }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -255,8 +120,151 @@ public class MainActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
+
+    private boolean dieIfRootUnavailable() {
+        if (!RootTools.isRootAvailable() || !RootTools.isAccessGiven()) {
+            AlertDialog ad = new AlertDialog.Builder(this).create();
+            ad.setCancelable(false); // This blocks the 'BACK' button
+            ad.setMessage(this.getString(R.string.no_root_message));
+            ad.setButton(this.getString(R.string.btOk), new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                    System.exit(0);
+                }
+            });
+            ad.show();
+        } else {
+            writeLog(this.getString(R.string.root_granted));
+            return true;
+        }
+        return false;
+    }
+
+
+    private void installButtonClicked() {
+        if (operationInProgress) {
+            writeLog(this.getString(R.string.op_in_progress));
+            return;
+        }
+        operationInProgress = true;
+        new InstallerTask().execute(!certsInstalled);
+    }
+
+    private void refreshInstallMode() {
+        if (certsInstalled) {
+            installButton.setText(R.string.del_cert_button_text);
+        } else {
+            installButton.setText(R.string.add_cert_button_text);
+        }
+    }
+
+    private void checkInstallSuccess() {
+        boolean certsInstalledOld = certsInstalled;
+        certsInstalled = checkCertInstalled();
+
+        if (certsInstalledOld != certsInstalled) {
+            writeLog(this.getString(R.string.finish_fine));
+        } else {
+            writeLog(this.getString(R.string.finish_error));
+        }
+
+        refreshInstallMode();
+    }
+
+    private void moveRawFileToSdCard(int resource, String targetFilename) throws IOException {
+        InputStream in = getResources().openRawResource(resource);
+        FileOutputStream out = new FileOutputStream(Environment.getExternalStorageDirectory() + "/" + targetFilename);
+
+        byte[] buff = new byte[1024];
+        int read;
+        while ((read = in.read(buff)) > 0) {
+            out.write(buff, 0, read);
+        }
+        in.close();
+        out.close();
+    }
+
+    private void executeCommand(String command) throws TimeoutException, RootDeniedException, IOException {
+        Log.w(packageName, getString(R.string.executing) + " \"" + command + "\"");
+
+        Command cmd = new Command(0, command) {
+            @Override
+            public void commandOutput(int i, String s) {
+                if (!s.isEmpty())
+                    Log.w(packageName, getString(R.string.command_output) + ": \"" + s + "\"");
+            }
+
+            @Override
+            public void commandTerminated(int i, String s) {
+                if (!s.isEmpty())
+                    Log.w(packageName, getString(R.string.command_terminated) + ": \"" + s + "\"");
+            }
+
+            @Override
+            public void commandCompleted(int i, int i2) {
+            }
+
+        };
+
+        RootTools.getShell(true).add(cmd);
+
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private boolean checkCertInstalled() {
+        File certFile1 = new File(CERTFILE1_PATH);
+        File certFile2 = new File(CERTFILE2_PATH);
+
+        if (!certFile1.exists() || !certFile2.exists()) {
+            return false;
+        }
+
+        String computedCertFile1Fingerprint = getThumbPrintString(certFile1);
+        String computedCertFile2Fingerprint = getThumbPrintString(certFile2);
+
+        writeLog(getString(R.string.fingerprint_for) + " " + certFile1.getName() + ": " + computedCertFile1Fingerprint);
+        writeLog(getString(R.string.fingerprint_for) + " " + certFile2.getName() + ": " + computedCertFile2Fingerprint);
+
+
+        if (!computedCertFile1Fingerprint.equalsIgnoreCase(CERTFILE1_FINGERPRINT) || !computedCertFile2Fingerprint.equalsIgnoreCase(CERTFILE2_FINGERPRINT)) {
+            writeLog(getString(R.string.fingerprints_invalid));
+            return false;
+        } else {
+            writeLog(getString(R.string.fingerprints_valid));
+        }
+
+        return true;
+    }
+
+    /*private String getFileMd5(File file) throws IOException, NoSuchAlgorithmException {
+        FileInputStream fileInputStream = new FileInputStream(file.getPath());
+        MessageDigest messageDigest = MessageDigest.getInstance("MD5");
+
+        //Using MessageDigest update() method to provide input
+        byte[] buffer = new byte[8192];
+        int numOfBytesRead;
+        while ((numOfBytesRead = fileInputStream.read(buffer)) > 0) {
+            messageDigest.update(buffer, 0, numOfBytesRead);
+        }
+
+        byte[] digestResult = messageDigest.digest();
+
+
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < digestResult.length; i++)
+            sb.append(Integer.toString((digestResult[i] & 0xff) + 0x100, 16).substring(1));
+
+
+        return sb.toString();
+    }*/
+
     private void writeLog(String text) {
-        textViewLog.setText(textViewLog.getText() + System.getProperty("line.separator") + System.getProperty("line.separator") + text);
+        textViewLog.setText(textViewLog.getText() + "\n\n" + text);
         final ScrollView sc = (ScrollView) findViewById(R.id.scrollView);
         sc.post(new Runnable() {
             @Override
@@ -264,6 +272,52 @@ public class MainActivity extends Activity {
                 sc.fullScroll(ScrollView.FOCUS_DOWN);
             }
         });
+
+        Log.w(this.getPackageName(), text);
+    }
+
+    public String getThumbPrint(X509Certificate cert) throws NoSuchAlgorithmException, CertificateEncodingException {
+        MessageDigest md = MessageDigest.getInstance("md5");
+        byte[] der = cert.getEncoded();
+        md.update(der);
+        byte[] digest = md.digest();
+        return hexify(digest);
+    }
+
+    public String getThumbPrintString(File certfile) {
+        FileInputStream is;
+        String output = "ERROR";
+
+        try {
+            is = new FileInputStream(certfile);
+            CertificateFactory x509CertFact = CertificateFactory.getInstance("X.509");
+            X509Certificate cert = (X509Certificate) x509CertFact.generateCertificate(is);
+            output = getThumbPrint(cert);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (CertificateException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+
+
+        return output;
+    }
+
+    public String hexify(byte bytes[]) {
+        char[] hexDigits = {'0', '1', '2', '3', '4', '5', '6', '7',
+                '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+
+        StringBuffer buf = new StringBuffer(bytes.length * 2 + bytes.length);
+
+        for (byte aByte : bytes) {
+            buf.append(hexDigits[(aByte & 0xf0) >> 4]);
+            buf.append(hexDigits[aByte & 0x0f]);
+            buf.append(':');
+        }
+
+        return buf.toString().substring(0, buf.toString().length() - 1);
     }
 
 
@@ -302,50 +356,53 @@ public class MainActivity extends Activity {
         }
 
         private void installCerts() throws IOException, TimeoutException, RootDeniedException {
-            publishProgress("writing temporary files to sdcard");
+            publishProgress(getString(R.string.write_tmp_file_sd));
 
             moveRawFileToSdCard(R.raw.cert_5ed36f99_0, "5ed36f99.0");
             moveRawFileToSdCard(R.raw.cert_e5662767_0, "e5662767.0");
 
-            publishProgress("remounting system partition for writing");
-            executeCommand("su -c 'mount -o remount,rw /system'");
+            publishProgress(getString(R.string.remounting_rw));
+            executeCommand("mount -o remount,rw /system");
 
-            publishProgress("copying certificates to system partition");
-            executeCommand("su -c 'cat " + Environment.getExternalStorageDirectory() + "/5ed36f99.0 > /system/etc/security/cacerts/5ed36f99.0'");
-            executeCommand("su -c 'cat " + Environment.getExternalStorageDirectory() + "/e5662767.0 > /system/etc/security/cacerts/e5662767.0'");
+            publishProgress(getString(R.string.copying_cert_files_to_system));
+            executeCommand("cat " + Environment.getExternalStorageDirectory() + "/5ed36f99.0 > /system/etc/security/cacerts/5ed36f99.0");
+            executeCommand("cat " + Environment.getExternalStorageDirectory() + "/e5662767.0 > /system/etc/security/cacerts/e5662767.0");
 
-            publishProgress("writing file permissions");
-            executeCommand("su -c 'chmod 644 /system/etc/security/cacerts/5ed36f99.0'");
-            executeCommand("su -c 'chmod 644 /system/etc/security/cacerts/e5662767.0'");
+            publishProgress(getString(R.string.writing_file_permissions));
+            executeCommand("chmod 644 /system/etc/security/cacerts/5ed36f99.0");
+            executeCommand("chmod 644 /system/etc/security/cacerts/e5662767.0");
 
-            publishProgress("remounting system partition to read only");
-            executeCommand("su -c 'mount -o remount,ro /system'");
+            publishProgress(getString(R.string.remounting_ro));
+            executeCommand("mount -o remount,ro /system");
+
+            publishProgress(getString(R.string.deleteing_tmp_files));
+            executeCommand("rm " + Environment.getExternalStorageDirectory() + "/5ed36f99.0");
+            executeCommand("rm " + Environment.getExternalStorageDirectory() + "/e5662767.0");
+
         }
 
-
         private void uninstallCerts() throws IOException, RootDeniedException, TimeoutException {
-            publishProgress("remounting system partition for writing");
-            executeCommand("su -c 'mount -o remount,rw /system'");
+            publishProgress(getString(R.string.remounting_rw));
+            executeCommand("mount -o remount,rw /system");
 
-            publishProgress("removing certificate files");
-            executeCommand("su -c 'rm /system/etc/security/cacerts/5ed36f99.0'");
-            executeCommand("su -c 'rm /system/etc/security/cacerts/e5662767.0'");
+            publishProgress(getString(R.string.removing_cert_files));
+            executeCommand("rm /system/etc/security/cacerts/5ed36f99.0");
+            executeCommand("rm /system/etc/security/cacerts/e5662767.0");
 
-            publishProgress("remounting system partition to read only");
-            executeCommand("su -c 'mount -o remount,ro /system'");
+            publishProgress(getString(R.string.remounting_ro));
+            executeCommand("mount -o remount,ro /system");
         }
 
         protected void onPostExecute(Void result) {
             try {
                 Thread.sleep(300);
             } catch (InterruptedException e) {
+                e.printStackTrace();
             }
 
             operationInProgress = false;
 
             checkInstallSuccess();
         }
-
-
     }
 }
