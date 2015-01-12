d7f635ac402efbe5455c66b0d82f0321af0d365c
Matthias Sieke <matthias@sieke.it>
2015-01-12 14:23:17 +0100

    move to Android Studio 1.01

---
+++ cacertorgcertificateinstaller/build/generated/source/buildConfig/release/it/sieke/android/cacertorgcertificateinstaller/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package it.sieke.android.cacertorgcertificateinstaller;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String PACKAGE_NAME = "it.sieke.android.cacertorgcertificateinstaller";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 2;
+  public static final String VERSION_NAME = "1.1 Birkenhead";
+}
